dcadcbf3e00867133a7f80284da6c3510a938399
Matt Elbé <matt.leboucher@gmail.com>
2018-12-14 17:18:57 +0100

    Raises exception instead of returning it.

--- utils/files.py
+++ utils/files.py
@@ -6,7 +6,7 @@ def retrieve_dataset_information(dataset_info_file_path):
     filename, file_extension = os.path.splitext(dataset_info_file_path)
 
     if not file_extension == ".csv":
-        return ValueError('You must provide the dataset information in a CSV file at its root.')
+        raise ValueError('You must provide the dataset information in a CSV file at its root.')
 
     class_names = []
     class_colors = []
