0ae264ec649c844a558d584ed1223322757fc953
Matt Elbé <matt.leboucher@gmail.com>
2018-12-17 18:21:08 +0100

    Adds some doc.

--- utils/builders.py
+++ utils/builders.py
@@ -68,6 +68,13 @@ class ModelBuilder:
         self.weights_directory = weights_directory
 
     def build(self, model_name, inputs):
+        """
+        Builds a model using the backbone the builder was provided with when instantiated.
+
+        :param model_name:  The name of the model to build.
+        :param inputs:      The inputs of the built model.
+        :return:            The network and its initialization function.
+        """
         if model_name not in available_models.keys():
             raise ValueError('Requested model {} is not available.'.format(model_name))
 
@@ -80,6 +87,14 @@ class ModelBuilder:
                                             is_training=self.is_training)
 
     def download_backbone_weights(self, backbone_name, only_if_not_exists=True):
+        """
+        Downloads the pretrained weights for the requested model and saves them into the weights directory
+        provided to the builder's instance.
+
+        :param backbone_name:       The name of the model of which to retrieve the pretrained weights.
+        :param only_if_not_exists:  Whether or not to download the weights if they already exist in the weights
+                                    directory.
+        """
         if only_if_not_exists and not Path(self.weights_directory).exists():
             from utils.download import download_model_weights
             download_model_weights(model_name=backbone_name, weights_directory=self.weights_directory)
