d558f04cc842b149e5d126bbf26b4bd154dfafd4
Matt Elbé <matt.leboucher@gmail.com>
2018-12-17 12:39:45 +0100

    Adds segmentation validation utils.

---
+++ utils/validation.py
@@ -0,0 +1,83 @@
+from sklearn.metrics import precision_score, recall_score, f1_score
+import numpy as np
+
+
+class SegmentationEvaluator:
+    def __init__(self, measure_names):
+        for measure_name in measure_names:
+            if measure_name not in self._get_available_measures_with_functions().keys():
+                raise ValueError('Measure {} is not supported for validation.'.format(measure_name))
+
+        self.active_measures = measure_names
+
+        self.measures_history = {}
+        self.initialize_history()
+
+    def initialize_history(self):
+        for measure_name in self.active_measures:
+            self.measures_history[measure_name] = []
+
+    def evaluate(self, prediction, annotation):
+        for measure_name in self.active_measures:
+            measure_function = self._get_available_measures_with_functions()[measure_name]
+            self.measures_history[measure_name] = measure_function(prediction, annotation)
+
+    def _get_available_measures_with_functions(self):
+        return {
+            'accuracy': self.compute_accuracy,
+            'accuracy_per_class': self.compute_accuracy_per_class,
+            'precision': precision_score,
+            'recall': recall_score,
+            'f1': f1_score,
+            'iou': self.compute_mean_iou,
+        }
+
+    @staticmethod
+    def compute_accuracy(prediction, annotation):
+        total = len(annotation)
+        count = 0.0
+
+        for i in range(total):
+            if prediction[i] == annotation[i]:
+                count = count + 1.0
+
+        return float(count) / float(total)
+
+    @staticmethod
+    def compute_accuracy_per_class(prediction, annotation, classes_count):
+        total = []
+        for val in range(classes_count):
+            total.append((annotation == val).sum())
+
+        count = [0.0] * classes_count
+        for i in range(len(annotation)):
+            if prediction[i] == annotation[i]:
+                count[int(prediction[i])] = count[int(prediction[i])] + 1.0
+
+        accuracies = []
+        for i in range(len(total)):
+            if total[i] == 0:
+                accuracies.append(1.0)
+            else:
+                accuracies.append(count[i] / total[i])
+
+        return accuracies
+
+    @staticmethod
+    def compute_mean_iou(prediction, annotation):
+        unique_labels = np.unique(annotation)
+        num_unique_labels = len(unique_labels)
+
+        intersection = np.zeros(num_unique_labels)
+        union = np.zeros(num_unique_labels)
+
+        for index, val in enumerate(unique_labels):
+            prediction_i = prediction == val
+            label_i = annotation == val
+
+            intersection[index] = float(np.sum(np.logical_and(label_i, prediction_i)))
+            union[index] = float(np.sum(np.logical_or(label_i, prediction_i)))
+
+
+        mean_iou = np.mean(intersection / union)
+        return mean_iou
