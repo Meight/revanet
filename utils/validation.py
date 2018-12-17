c3af3a24ac58e32ea7a9bc37c05e2347f6f6fdfa
Matt Elbé <matt.leboucher@gmail.com>
2018-12-17 18:36:34 +0100

    Adds docs.

--- utils/validation.py
+++ utils/validation.py
@@ -6,6 +6,15 @@ import numpy as np
 
 class SegmentationEvaluator:
     def __init__(self, measure_names, number_of_classes):
+        """
+        A segmentation evaluator which can compute a set of measures when given a prediction and an associated
+        annotation.
+
+        :param measure_names:       A list of supported measures to compute every time the evaluator is fed with a
+                                    couple of a prediction and its annotation.
+        :param number_of_classes:   The number of classes in the dataset. Required for the computation of some
+                                    of the provided measures.
+        """
         for measure_name in measure_names:
             if measure_name not in self._get_available_measures_with_functions().keys():
                 raise ValueError('Measure {} is not supported for validation.'.format(measure_name))
@@ -17,15 +26,34 @@ class SegmentationEvaluator:
         self.initialize_history()
 
     def initialize_history(self):
+        """
+        Resets the history held by the evaluator. Should be done after every epoch.
+        """
         for measure_name in self.active_measures:
             self.measures_history[measure_name] = []
 
     def evaluate(self, prediction, annotation):
+        """
+        Evaluates the activated measures onto the provided prediction and annotation. The computed measures
+        are then stored within the internal evaluator's history and accessible at any time until the history
+        gets cleared explicitly.
+
+        :param prediction: The predicted image.
+        :param annotation: The annotation image.
+        """
         for measure_name in self.active_measures:
             measure_function = self._get_available_measures_with_functions()[measure_name]
             self.measures_history[measure_name] = measure_function(prediction, annotation)
 
     def get_averaged_measures(self, current_epoch):
+        """
+        Computes the average of every activated measure.
+
+        :param current_epoch:   The epoch that corresponds to the moment the averages are computed. This epoch is
+                                prepended to any measure for results formatting.
+        :return: An ordered dictionary of the averaged activated measures, prepended by the epoch at which these
+                 were calculated.
+        """
         averaged_measures = OrderedDict({'epoch': current_epoch})
 
         for measure_name in self.active_measures:
@@ -36,6 +64,10 @@ class SegmentationEvaluator:
         return averaged_measures
 
     def _get_available_measures_with_functions(self):
+        """
+        :return: A dictionary of the measures the evaluator is able to compute, mapping their name to their actual
+                 implementation.
+        """
         return {
             'accuracy': self.compute_accuracy,
             'accuracy_per_class': self.compute_accuracy_per_class,
