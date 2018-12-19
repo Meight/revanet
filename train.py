Fixes weights dimension in loss computation.

train.py
@@ -168,8 +168,8 @@ adapted_loss = tf.nn.sigmoid_cross_entropy_with_logits \
     if IS_MULTI_LABEL_CLASSIFICATION else tf.nn.softmax_cross_entropy_with_logits_v2
 
 loss = tf.reduce_mean(tf.losses.compute_weighted_loss(weights=tf.cast(unc, tf.float32),
-                                                      losses=adapted_loss(logits=predictions_tensor,
-                                                                          labels=output_tensor)))
+                                                      losses=tf.reduce_mean(adapted_loss(logits=predictions_tensor,
+                                                                                         labels=output_tensor))))
 
 opt = tf.train.RMSPropOptimizer(learning_rate=LEARNING_RATE,
                                 decay=0.995,