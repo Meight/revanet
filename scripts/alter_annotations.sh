7398b3e28f7d6357ded5fb32f6ec90e3cdee0fbf
Matt Elbé <matt.leboucher@gmail.com>
2019-01-08 12:58:49 +0100

    Adds launch Shell scripts for annotation alteration.

---
+++ scripts/alter_annotations.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=alter-annotations
+#SBATCH --output=/projets/thesepizenberg/deep-learning/logs/alter-%j.out
+#SBATCH --error=/projets/thesepizenberg/deep-learning/logs/alter-%j.out
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --partition=24CPUNodes
+#SBATCH --mem-per-cpu=7500M
+
+
+set -e
+
+# Various script and dataset paths.
+PYTHON_PATH="/users/thesepizenberg/mlebouch/venv/bin"
+SCRIPT_PATH="/projets/thesepizenberg/deep-learning/revanet/datasets"
+TEXT_FILES_DIRECTORY="/projets/thesepizenberg/deep-learning/datasets/VOC2012-fresh"
+DATASET_NAME="augmented-pascal"
+TARGET_DATASET_ROOT="/projets/thesepizenberg/deep-learning/revanet/datasets"
+DATASET_ROOT_PATH="/projets/thesepizenberg/deep-learning/revanet/datasets"
+ANNOTATIONS_PATH="${DATASET_ROOT_PATH}/train_labels"
+TARGET_PATH=$DATASET_ROOT_PATH
+
+# Begin script.
+srun ${PYTHON_PATH}/python \
+               "$SCRIPT_PATH/alter_annotations.py" \
+               --annotations-paths=${ANNOTATIONS_PATH} \
+               --dataset-root-path=${DATASET_ROOT_PATH} \
+               --target-path=${TARGET_PATH} \
+               --transformation=${0} \
+               --target-average-miou=${1} \
+               --precision-tolerance=${2}
+wait
---
+++ scripts/launch_alterations.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+WORKING_DIR=pwd
+TRANSFORMATIONS=("erosion", "dilation")
+PRECISION_TOLERANCE=0.09
+
+for transformation in "${TRANSFORMATIONS[@]}"; do
+    current_target_miou=60
+    step=2
+    upper_bound=98
+
+    while (( current_target_miou <= 98 )); do
+        srun ${WORKING_DIR}/alter_annotations.sh ${transformation} ${current_target_miou} ${PRECISION_TOLERANCE}
+        echo "Launched annotations alterations with transformation '${transformation}' and target mIOU of ${current_target_miou}."
+    done
+done
