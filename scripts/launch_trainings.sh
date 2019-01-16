3c0650d05a8962a2b18cb1b8dc2c8066adcfed22
Meight <matt.leboucher@gmail.com>
2019-01-17 00:16:38 +0100

    Prepares scripts for execution onto normal augmnted Pascal

--- scripts/launch_trainings.sh
+++ scripts/launch_trainings.sh
@@ -4,22 +4,25 @@ DATASETS_PATH="/projets/thesepizenberg/deep-learning/revanet/datasets"
 SCRIPT_PATH="/projets/thesepizenberg/deep-learning/revanet/scripts"
 MODEL_NAMES=("BiSeNet") # "FC-DenseNet103" "GCN" "DeepLabV3_plus" "RefineNet" "DenseASPP" "PSPNet" "DDSC" "AdapNet")
 FRONTEND_NAMES=("ResNet101")
-INPUT_SIZE=(256)
-BATCH_SIZE=(1)
-DATASET="augmented-pascal-altered"
+INPUT_SIZE=(512)
+BATCH_SIZE=(1 2)
+LEARNING_RATES=(0.01 0.05 0.001 0.005)
+DATASET="augmented-pascal"
 
 ALTERATION="erosion"
 
-TRAIN_FOLDERS=(${DATASETS_PATH}/$DATASET/$ALTERATION*)
-#TRAIN_FOLDERS=("train")
+# TRAIN_FOLDERS=(${DATASETS_PATH}/$DATASET/$ALTERATION*)
+TRAIN_FOLDERS=("train_annotations")
 
 for input_size in "${INPUT_SIZE[@]}"; do
     for batch_size in "${BATCH_SIZE[@]}"; do
         for model_name in "${MODEL_NAMES[@]}"; do
             for frontend_name in "${FRONTEND_NAMES[@]}"; do
-                for ((i=0; i<${#TRAIN_FOLDERS[@]}; i++)); do
-                    sbatch "$SCRIPT_PATH"/train.sh ${model_name} ${frontend_name} ${input_size} ${batch_size} ${DATASET} "$(basename ${TRAIN_FOLDERS[$i]})"
-                done
+		for learning_rate in "${LEARNING_RATES[@]}"; do
+                    for ((i=0; i<${#TRAIN_FOLDERS[@]}; i++)); do
+                        sbatch "$SCRIPT_PATH"/train.sh ${model_name} ${frontend_name} ${input_size} ${batch_size} ${DATASET} "$(basename ${TRAIN_FOLDERS[$i]})" ${learning_rate}
+		    done;
+                done;
             done;
         done;
     done;
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=bisenet
-#SBATCH --output=/projets/thesepizenberg/deep-learning/logs/bisenet-%j.out
-#SBATCH --error=/projets/thesepizenberg/deep-learning/logs/bisenet-%j.out
+#SBATCH --job-name=bisenet-gpu
+#SBATCH --output=/projets/thesepizenberg/deep-learning/logs/gpu-bisenet-%j.out
+#SBATCH --error=/projets/thesepizenberg/deep-learning/logs/gpu-bisenet-%j.out
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
@@ -20,8 +20,10 @@ VENV_PATH=$TRAIN_SCRIPT_DIR
 
 # Create a virtual environment from the Docker container.
 
-# srun keras-py3-tf virtualenv --system-site-packages ${VENV_PATH}
-# wait
+srun keras-py3-tf virtualenv --system-site-packages ${VENV_PATH}
+wait
+
+# srun keras-py3-tf ${VENV_PATH}/bin/pip install -r ${TRAIN_SCRIPT_DIR}/requirements.txt
 
 echo "Launching with on train folder: ${6}"
 srun keras-py3-tf ${VENV_PATH}/bin/python "$TRAIN_SCRIPT_DIR/train.py" \
@@ -31,7 +33,8 @@ srun keras-py3-tf ${VENV_PATH}/bin/python "$TRAIN_SCRIPT_DIR/train.py" \
                 --batch-size=${4} \
                 --dataset-name=${5} \
                 --train-annotations-folder=${6} \
-                --number-of-epochs=50 \
+		--learning-rate=${7} \
+                --number-of-epochs=75 \
                 --save-weights-every=24 \
                 --validate-every=1
 wait
