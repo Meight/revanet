57b994866420c66e4ae5c993d63d43b8c4279793
Meight <matt.leboucher@gmail.com>
2019-01-18 18:02:53 +0100

    Tweaks multi GPU training Shell script

--- scripts/launch_trainings_multi.sh
+++ scripts/launch_trainings_multi.sh
@@ -4,14 +4,15 @@ DATASETS_PATH="/projets/thesepizenberg/deep-learning/revanet/datasets"
 SCRIPT_PATH="/projets/thesepizenberg/deep-learning/revanet/scripts"
 MODEL_NAMES=("BiSeNet") # "FC-DenseNet103" "GCN" "DeepLabV3_plus" "RefineNet" "DenseASPP" "PSPNet" "DDSC" "AdapNet")
 FRONTEND_NAMES=("ResNet101")
-INPUT_SIZE=(256)
-BATCH_SIZE=(4)
-DATASET="augmented-pascal-altered"
+INPUT_SIZE=(384)
+BATCH_SIZE=(1)
+DATASET="augmented-pascal"
 
 ALTERATION="erosion"
 
 # TRAIN_FOLDERS=(${DATASETS_PATH}/$DATASET/$ALTERATION*)
-TRAIN_FOLDERS=("erosion_90.00_95.68")
+# TRAIN_FOLDERS=("erosion_90.00_95.68")
+TRAIN_FOLDERS=("train_annotations")
 
 for input_size in "${INPUT_SIZE[@]}"; do
     for batch_size in "${BATCH_SIZE[@]}"; do
